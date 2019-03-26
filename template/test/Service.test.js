663a2c092c0af285c0ca42aab1df9804ae106f2d
Chakkrit <chakkrit@livesync.io>
2019-03-26 12:54:00 +0700

    Update Service.test.js

--- template/test/Service.test.js
+++ template/test/Service.test.js
@@ -4,18 +4,17 @@ const kii = require('kiix');
 const env = require('../config');
 
 kii.boot(env);
+log4js.configure({
+    appenders: {
+        console: { type: 'console' }
+    },
+    categories: { default: { appenders: ['console'], level: 'all' } }
+})
 
+let loggger = log4js.getLogger()
 let expect = chai.expect
 
 describe('## Test section', function() {
-    log4js.configure({
-        appenders: {
-            console: { type: 'console' }
-        },
-        categories: { default: { appenders: ['console'], level: 'all' } }
-    })
-    let loggger = log4js.getLogger()
-
     it("=>test function", async function () {
         var result = await S._server.getConfig()
         //loggger.debug(result.port)
