f447ad6ba22fbf19e246e7b6cf53f4cda8dcd49b
Chakkrit <chakkrit@livesync.io>
2019-03-26 11:52:03 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-exports.test = function(config) {
+exports.boot = function(config) {
     const express = require('express')
     const app = express()
     global.DOCS = {
@@ -17,37 +17,21 @@ exports.test = function(config) {
     require('./init/middles.js'); // middle
     require('./init/boot.js')(app); // model
     require('./init/services.js'); // service
+    return app
 }
 
 exports.init = function(config) {
-    const express = require('express')
     const next = require('next')
     const http = require('http')
     const dev = process.env.NODE_ENV !== 'production'
     const ini = next({ dev })
     const handle = ini.getRequestHandler()
-
+    const app =exports.boot(config)
     ini.prepare()
         .then(() => {
-            const app = express()
+            
             const server = http.createServer(app);
-        
-            global.DOCS = {
-                api:{},
-                socket:{},
-            };
-            global.C = config; //config
-            global.L = {}; //local session
-            global.D = {}; //data base
-            global.M = {}; //middle where
-            global.S = {}; //service
-            global.R = require('./init/cache.js'); //redis
-            global.F = require('./init/funcs.js'); //function
 
-            require('./init/models.js'); // model
-            require('./init/middles.js'); // middle
-            require('./init/boot.js')(app); // model
-            require('./init/services.js'); // service
             require('./init/routes.js')(app, ini); // router
             require('./init/socket.js')(server);
 
