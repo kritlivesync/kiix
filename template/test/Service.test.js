ddfc638ba77aa59e0489953e7e86d1c7690492c8
Chakkrit <chakkrit@livesync.io>
2019-03-26 12:25:59 +0700

    Create Service.test.js

---
+++ template/test/Service.test.js
@@ -0,0 +1,24 @@
+const chai = require('chai');
+const log4js = require('log4js');
+const kii = require('kiix');
+const env = require('../config');
+
+kii.boot(env);
+
+let expect = chai.expect
+
+describe('## Test section', function() {
+    log4js.configure({
+        appenders: {
+            console: { type: 'console' }
+        },
+        categories: { default: { appenders: ['console'], level: 'all' } }
+    })
+    let loggger = log4js.getLogger()
+
+    it("=>test function", async function () {
+        var result = await S._server.getConfig()
+        //loggger.debug(result.port)
+        expect(result).to.have.property('port').equal(36996)
+    })
+})
